37f08d0dd75efae66d79bf6181f7b7b074d34914
rajesh <krajeshgowri7032@mail.com>
2022-08-21 19:18:12 +0530

    Javascript-Nummethodes

---
+++ tostring.js
@@ -0,0 +1,36 @@
+// // tostring
+
+var num = 10;
+console.log(typeof(num));
+console.log(num.toString());
+console.log(typeof(num.toString()));
+
+
+//parsefloat()
+
+// var strNum= '10.5';
+// console.log(strNum);
+// console.log(typeof(strNum));
+// console.log(parseFloat(strNum));
+
+
+// parseInt()
+
+// var str = "5";
+// console.log(typeof(str));
+// console.log(parseInt(str));
+// console.log(typeof(parseInt(str)));
+
+
+//toFixed
+
+// var num =10.8374939479
+// console.log(num);
+// console.log(num.toFixed(3));
+// console.log(typeof (num));
+// console.log(num).toFixed(11);
+
+
+
+
+
